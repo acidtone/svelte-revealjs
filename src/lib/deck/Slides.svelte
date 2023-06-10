e1436f5b9983eb281656ab281708d867f18b5698
Tony Grimes <acidtone@tonygrimes.com>
2023-06-10 13:59:00 -0600

    Configured, empty revealj project

---
+++ src/lib/deck/Slides.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { onMount } from 'svelte'
+
+  import Reveal from 'reveal.js'
+	import Highlight from 'reveal.js/plugin/highlight/highlight'
+	import Markdown from 'reveal.js/plugin/markdown/markdown'
+	import Notes from 'reveal.js/plugin/notes/notes'
+
+	import 'reveal.js/dist/reveal.css'
+	import 'reveal.js/dist/theme/black.css'
+	import 'reveal.js/plugin/highlight/monokai.css'
+
+	onMount(() => {
+		const deck = new Reveal({
+			plugins: [Markdown, Highlight, Notes],
+      autoAnimateEasing: 'ease',
+			autoAnimateDuration: 1,
+			hash: true
+			// controls: false,
+			// progress: false
+
+		})
+
+		deck.initialize()
+	})
+
+</script>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Slides from '$lib/deck/Slides.svelte'
+	import '../app.postcss';
+</script>
+
+<svelte:head>
+	<title>Presentation</title>
+</svelte:head>
+<Slides />
+
