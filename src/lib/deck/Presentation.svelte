4e5265d526aa84ad29796fc83c22f715e3cc5cc1
Tony Grimes <acidtone@tonygrimes.com>
2023-06-10 15:42:30 -0600

    added basic Code component

--- src/lib/deck/Presentation.svelte
+++ src/lib/deck/Presentation.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
-  import Slide from './Slide.svelte'
+  import Slide from './Slide.svelte';
 	import Slides from './Slides.svelte';
+  import Code from './Code.svelte';
 </script>
 
 <Slide animate>
@@ -15,4 +16,20 @@
 <Slide>
   <Slide>Vertical Slide 1</Slide>
   <Slide>Vertical Slide 2</Slide>
+</Slide>
+<Slide animate>
+  <Code id="code" lines>
+    {`
+      function love() {}
+    `}
+  </Code>
+</Slide>
+<Slide animate>
+  <Code id="code" lines>
+    {`
+      function love() {
+        console.log('Svelte');
+      }
+    `}
+  </Code>
 </Slide>
