ee1b256cf2d7bb2bb2bda50aff6e8e047cfc56f5
Tony Grimes <acidtone@tonygrimes.com>
2023-06-10 16:36:27 -0600

    Added basic transition example

--- src/lib/deck/Presentation.svelte
+++ src/lib/deck/Presentation.svelte
@@ -33,3 +33,21 @@
     `}
   </Code>
 </Slide>
+
+<Slide animate>
+  <p class="uppercase">Transitions</p>
+  <div class="flex gap-2 place-content-center">
+    <div data-id="box-1" class="w-[100px] h-[100px] bg-gray-400"></div>
+    <div data-id="box-2" class="w-[100px] h-[100px] bg-gray-400"></div>
+    <div data-id="box-3" class="w-[100px] h-[100px] bg-gray-400"></div>
+  </div>
+</Slide>
+
+<Slide animate>
+  <p class="uppercase">Transitions</p>
+  <div class="flex gap-2 place-content-center">
+    <div data-id="box-1" class="w-[200px] h-[200px] bg-red-400"></div>
+    <div data-id="box-2" class="w-[200px] h-[200px] bg-green-400"></div>
+    <div data-id="box-3" class="w-[200px] h-[200px] bg-blue-400"></div>
+  </div>
+</Slide>
