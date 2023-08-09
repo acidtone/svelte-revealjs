b86c3e59afb835f396290b878fa4a6a0f76f17f5
Tony Grimes <acidtone@tonygrimes.com>
2023-08-09 17:04:54 -0600

    added code copy paste

---
+++ src/content/slides/decks/copy-test.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import Slide from '$lib/components/Slide.svelte';
+	import Code from '$lib/components/Code.svelte';
+	import Markdown from '$lib/components/Markdown.svelte';
+	import Notes from '$lib/components/Notes.svelte';		
+	import MdFragment from '$lib/components/MDFragment.svelte';
+	import Fragment from '$lib/components/Fragment.svelte';
+
+</script>
+
+<Slide>
+	<p>Code Block</p>
+	<Code id="code">
+		{`
+      function love() {}
+    `}
+	</Code>
+</Slide>
+
+<Slide animate>
+	<p>Animated Code Block</p>
+	<Code id="code" lines>
+		{`
+      function love() {}
+    `}
+	</Code>
+</Slide>
+<Slide animate>
+	<p>Animated Code Block</p>
+	<Code id="code" lines>
+		{`
+      function love() {
+        console.log('Svelte');
+      }
+    `}
+	</Code>
+</Slide>
+
+<Markdown>
+	{`
+    ## Markdown
+    ### Example
+    \`\`\`js [1-2|3|4]
+    let a = 1;
+    let b = 2;
+    let c = x => 1 + 2 + x;
+    c(3);
+    \`\`\`
+  `}
+</Markdown>
--- src/routes/slides/[slug]/+page.svelte
+++ src/routes/slides/[slug]/+page.svelte
@@ -5,17 +5,20 @@
 	import Highlight from 'reveal.js/plugin/highlight/highlight';
 	import Markdown from 'reveal.js/plugin/markdown/markdown';
 	import Notes from 'reveal.js/plugin/notes/notes';
+	import CopyCode from '$lib/plugins/copycode.esm';
 
 	import 'reveal.js/dist/reveal.css';
 	import 'reveal.js/dist/theme/black.css';
 	import 'reveal.js/plugin/highlight/monokai.css';
+	import '$lib/plugins/copycode.css';
 	import '../../../app.postcss';
 
 	export let data;
+
 	
 	onMount(() => {
 		const deck = new Reveal({
-			plugins: [Markdown, Highlight, Notes],
+			plugins: [Markdown, Highlight, Notes, CopyCode],
 			autoAnimateEasing: 'ease',
 			autoAnimateDuration: 1,
 			hash: true
